b47e7907dbec99884109dc66c450fe45f7c38405
DanieleBaiocco <daniele.baiocco@studio.unibo.it>
2026-04-23 17:04:37 +0200

    definizione delle due root di base: ricerca parola e aggiunta parola

--- controller/api.py
+++ controller/api.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import Depends, HTTPException, Response, status
+from fastapi import FastAPI
 from sqlalchemy.orm import Session
 
 from database.engine import SessionLocal
-from repositories.parola_dao import ParolaDAO
+from schemas.associazione import AddAssociazioneRequest
 from schemas.parola import ParolaRead
 from services.service import ParolaService
 
@@ -16,10 +17,33 @@ def get_db():
     finally:
         db.close()
 
+"""
+    Vien fatta quando l'utente cerca una certa parola che ha visto durante la lettura.
+    Vengono ritornati tutti gli omonimi flaggati a True se sono nel DB, a False altrimenti
+"""
 @app.get("/parola/{parola}")
-def create_termine(parola: str, username: str, db: Session = Depends(get_db)) -> ParolaRead:
+def get_parola(username: str, parola: str, db: Session = Depends(get_db)) -> list[ParolaRead]:
     service = ParolaService(db)
     try:
-        return service.create_parola(parola, username)
+        return service.get_parola(username, parola)
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+"""
+    Vien fatta quando l'utente decide di aggiungere al proprio vocabolario personale
+    le parole. Questo vien fatto dopo get_parola(), nella richiesta ci sono le parole
+    in versione Treccani da aggiungere es. [palla1, palla2, palla3]  
+"""
+@app.post("/parole/associazioni", status_code=status.HTTP_204_NO_CONTENT)
+def add_associazione(
+    payload: AddAssociazioneRequest,
+    db: Session = Depends(get_db)
+):
+    service = ParolaService(db)
+    try:
+        service.add_associazione(payload.username, payload.parole)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
     except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
